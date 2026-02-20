4050021814a4e11830e1425ef39162ae98a9a104
paizonx <paizonx@gmail.com>
2026-02-21 02:02:10 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+// ==========================================
+// DATABASE VIDEO
+// Masukkan ID Video Videy Anda di sini.
+// Bisa dengan ekstensi (contoh: "abc.mp4", "xyz.mov") 
+// atau tanpa ekstensi (contoh: "abc").
+// ==========================================
+
+const videoList = [
+    "aqwvCVWv1",
+    "QlXarW9J1",
+    "9bgq8gbW1",
+    "yzgarDr91",
+    "T0qxWRCe1",
+    "eBYn6LJV1",
+    "nwnk9H0y1",
+    "oRjlHAto1",
+    "NeQusurl1",
+    "0uJcA7hW1",
+    "zVibXvsu1",
+    "C7M9mpu61",
+    "ZBAPcSA31",
+    "GGzKbwS51",
+    "0SUZgerE1",
+    "WMfWZq6E1",
+    "q7RvF1UH1",
+    "3E0XKiIF1",
+    "RYLODxim1",
+    "O0vuWEW21",
+    "ijl7cKN21",
+    "aYzgr0eU1",
+    "q2EzOKan1",
+    "S26wiYFH1",
+    "0vnD2eRP1",
+    "ssd4keuh1",
+    "jXrct0ZX1",
+    "YUcY4kmy2",
+    "iWlvGiwC1",
+    "iV6eWvhw2",
+    "3bdOAeE81",
+    "4LKYH6JS1",
+    "4U1rWLt11",
+];
