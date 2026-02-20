6da163a833b212db0bf82bd59f6b555c7d2c0ac4
paizonx <paizonx@gmail.com>
2026-02-21 02:05:20 +0700

    Remove duplicate comments and clean up videoList

--- data.js
+++ data.js
@@ -8,7 +8,6 @@
 const videoList = [
     "aqwvCVWv1",
     "QlXarW9J1",
-    "9bgq8gbW1",
     "yzgarDr91",
     "T0qxWRCe1",
     "eBYn6LJV1",
@@ -33,10 +32,9 @@ const videoList = [
     "0vnD2eRP1",
     "ssd4keuh1",
     "jXrct0ZX1",
-    "YUcY4kmy2",
     "iWlvGiwC1",
-    "iV6eWvhw2",
     "3bdOAeE81",
     "4LKYH6JS1",
     "4U1rWLt11",
+
 ];
